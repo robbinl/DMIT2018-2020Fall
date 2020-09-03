6c0b367f92348333b05e02a16510781cc900c292
robbinl <robbinl@telusplanet.net>
2020-09-03 10:15:35 -0600

    Setup PL02 to the same as PL01, it runs

---
+++ WebApp/SamplePages/PL02_DDL_Artists_to_CGV_AlbumsByArtistId.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp.SamplePages
+{
+    public partial class PL02_DDL_Artists_to_CGV_AlbumsByArtistId : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void LB_FetchArtistInfo_Click(object sender, EventArgs e)
+        {
+            if (DLL_Artists.SelectedValue == "0")
+                MessageLabel.Text = "Please Select an artist";
+            else
+            {
+                var selectedindex = DLL_Artists.SelectedIndex;
+                var selectedvalue = DLL_Artists.SelectedValue;
+                var selecteditem = DLL_Artists.SelectedItem;
+                MessageLabel.Text = $"Selected Index: {selectedindex}   " +
+                                    $"--Selected Value: {selectedvalue}   " +
+                                    $"--Selected Item: {selecteditem}";
+            }
+        }
+    }
+}
